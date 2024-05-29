2ddfa1917840983ea3dd754663ea369040a9b15c
rigauxh <heloise.rigaux@etu.umontpellier.fr>
2024-05-29 17:25:28 +0200

    Nav bar

--- SiteWeb/www/general/nav.php
+++ SiteWeb/www/general/nav.php
@@ -8,9 +8,11 @@ $fillInTheBlanks = ""; ?>
     <div class="enfants">
         <h2><a href="">Nos espaces de coworking</a></h2>
         <h2><a href="">Nos offres</a></h2>
-        <h2><a href="">Contact</a></h2>
+        <h2><a href="">Nous contacter</a></h2>
     </div>
     <div class="login">
-        <img src="../STYLE/icons/login.png"  alt="login image"/>
+        <a href="">
+            <img src="../STYLE/icons/login.png"  alt="login image"/>
+        </a>
     </div>
 </nav>
