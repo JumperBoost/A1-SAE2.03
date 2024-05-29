bfe4c847ee212e08e197d6f4f56d9184235c9812
Desal-42 <alexandre.deschanel.666@gmail.com>
2024-05-29 22:54:44 +0200

    push

--- SiteWeb/www/index.php
+++ SiteWeb/www/index.php
@@ -17,29 +17,30 @@
                 <div class = "menu">
                     <?php
                         include("general/connect.php");
-                        $SallesM = mysqli_query($connect, "SELECT * FROM Salles NATURAL JOIN Batiments ORDER BY ville");
-                        $i = 0;
-                    if(!count($SallesM)){
-                    } else {
-
-                    foreach($SallesM as $SalleM):
-                    ?>
-                    <a class="menuF" href="menuF.php?idFormule=<?=$SalleM['idSalle']?>">
-                        <div>
-                            <div class="image"> <img class="image" src="../IMAGES/<?=$SalleM['nomSalle']?>.jpg"> </div>
-                            <div class="text">
-                                <h2><?=$SalleM['nomSalle']?></h2>
-                                <h3>Capacité : <?=$SalleM['capaciteSalle']?></h3>
-                                <h3><?=$SalleM['prixJournnee']?> €</h3>
-                                <p>
-                                    Description :
-                                    <br>
-                                    <?=$SalleM['descriptionSalleCourte']?> </p>
-                            </div>
-                        </div>
-                    </a>
-                    <?php endforeach ;}?>
-
+                        $Salles = mysqli_query($connect, "SELECT * FROM sae.salles LEFT JOIN sae.batiments on salles.idBatiment = batiments.idBatiment ORDER BY ville, idSalle");
+                        if(count($Salles)){
+                        $ville = "";
+                        foreach($Salles as $Salle):
+                            ?>
+                            <?php if ($ville != $Salle['ville']){
+                                $ville =  $Salle['ville'];
+                                echo "</br><h1>$ville</h1></br>";
+                        }?>
+                            <a class="menuF" href="menuF.php?idFormule=<?=$Salle['idSalle']?>">
+                                <div>
+                                    <div class="image"> <img class="image" src="../IMAGES/<?=$Salle['nomSalle']?>.jpg"> </div>
+                                    <div class="text">
+                                        <h2><?=$Salle['nomSalle']?></h2>
+                                        <h3>Capacité : <?=$Salle['capaciteSalle']?></h3>
+                                        <h3><?=$Salle['prixJournnee']?> €</h3>
+                                        <p>
+                                            Description :
+                                            <br>
+                                            <?=$Salle['descriptionSalleCourte']?> </p>
+                                    </div>
+                                </div>
+                            </a>
+                        <?php endforeach ;}?>
                 </div>
             </section>
         </main>
